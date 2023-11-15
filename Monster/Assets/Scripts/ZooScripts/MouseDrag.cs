3a56c55858fc6021c693cbaab1d3e84e0e6af04d
habbydelvin <delvinfoo@habby.com>
2023-11-15 10:57:06 +0800

    TreeDeath+Minor Zoo Changes

--- Monster/Assets/Scripts/ZooScripts/MouseDrag.cs
+++ Monster/Assets/Scripts/ZooScripts/MouseDrag.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseDrag : MonoBehaviour
-{ 
-private Vector3 ResetCamera;
-private Vector3 Origin;
-private Vector3 Diference;
-private bool Drag = false;
-void Start()
 {
-    ResetCamera = Camera.main.transform.position;
-}
+    private Vector3 ResetCamera;
+    private Vector3 Origin;
+    private Vector3 Difference;
+    private bool Drag = false;
+
+    public float minYLimit = 0f; // Minimum Y position limit
+    public float maxYLimit = 10f; // Maximum Y position limit
+
+    void Start()
+    {
+        ResetCamera = Camera.main.transform.position;
+    }
+
     void LateUpdate()
     {
         if (Input.GetMouseButton(0))
         {
-            // Calculate the difference between the current mouse position and the camera's position
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Diference = new Vector3(0f, mousePosition.y - Camera.main.transform.position.y, 0f);
+            Difference = new Vector3(0f, mousePosition.y - Camera.main.transform.position.y, 0f);
 
             if (!Drag)
             {
-                // Start the drag operation
                 Drag = true;
                 Origin = mousePosition;
             }
@@ -34,11 +37,15 @@ void Start()
 
         if (Drag)
         {
-            // Only update the Y component of the camera's position
-            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Origin.y - Diference.y, Camera.main.transform.position.z);
+            float newY = Origin.y - Difference.y;
+
+            // Clamp the Y position within the specified range
+            newY = Mathf.Clamp(newY, minYLimit, maxYLimit);
+            Debug.Log("Stop Screen");
+
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, newY, Camera.main.transform.position.z);
         }
 
-        // RESET CAMERA TO STARTING POSITION WITH RIGHT CLICK
         if (Input.GetMouseButton(1))
         {
             Camera.main.transform.position = ResetCamera;
