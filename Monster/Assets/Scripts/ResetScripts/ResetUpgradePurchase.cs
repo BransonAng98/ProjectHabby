4d7c600118fcc63ec03998df6b0d552490278bad
KeithHabby <keithwong@habby.com>
2023-12-22 13:18:28 +0800

    Update
    New upgrade system implemented

---
+++ Monster/Assets/Scripts/ResetScripts/ResetUpgradePurchase.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetUpgradePurchase : MonoBehaviour
+{
+    public PlayerStatScriptableObject playerData;
+    public ResourceScriptableObject resourceData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ResetUpgrade()
+    {
+        resourceData.currentGold = 10000;
+        playerData.upgradeLevel = 1;
+        PlayerPrefs.DeleteAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
