2d1713e72b8c88710c77b221935a40e3054bbd95
habbydelvin <delvinfoo@habby.com>
2024-02-19 17:52:15 +0800

    blank zones follow gridspeed

--- Monster/Assets/GridMoveUpScript.cs
+++ Monster/Assets/GridMoveUpScript.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class GridMoveUpScript : MonoBehaviour
 {
     [SerializeField]
-    private int movespeed;
+    private float movespeed;
+    public GridSpawner gridspawnerScript;
+    public PlayerEndlessRunnerController player;
+
     // Start is called before the first frame update
     void Start()
     {
-        movespeed = 10;
+        gridspawnerScript = GameObject.Find("GridSpawner").GetComponent<GridSpawner>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerEndlessRunnerController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player.canMove)
+        {
+            moveGrid();
+        }
+    }
+
+    public void moveGrid()
+    {
+        movespeed = gridspawnerScript.gridSpeed;
         transform.Translate(Vector2.up * movespeed * Time.deltaTime);
     }
 }
